3fcb0f9740e99d21f8ccc683fecde6ff1a787ea6
nomayen <nomayen.ohin@gmail.com>
2023-11-26 14:40:26 +0600

    firebase and route diclare

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA5VmYRIt3nV3kskh8T8k7HMYE-8qh6GGI",
+    authDomain: "simple-firebase-570cd.firebaseapp.com",
+    projectId: "simple-firebase-570cd",
+    storageBucket: "simple-firebase-570cd.appspot.com",
+    messagingSenderId: "964568329763",
+    appId: "1:964568329763:web:c752617edcf23aa0a28b96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
