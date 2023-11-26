d6bb4ee82196e83a31fb1189d460be7b5f08c0ee
nomayen <nomayen.ohin@gmail.com>
2023-11-26 19:08:42 +0600

    github signin and signout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,53 @@
 import '../../App.css'
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
-
-
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = () => {
+    const notify = () => toast("User SignOut Successfully 🥳");
+    const notifySignin = () => toast("User SignIn Successfully 🥳");
+    const [user , setUser] = useState(null)
     const auth = getAuth(app)
-    const provider = new GoogleAuthProvider()
+    const GoogleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
+
+
+    const handleGithubLogin = () =>{
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setUser(loggedUser)
+        })
+        .catch(error=>{
+            console.log('error', error.message);
+        })
+    }
 
     const handleGoogleLogin= () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, GoogleProvider)
         .then(result =>{
-            const user = result.user;
-            console.log(user);
+            const LoggedInUser = result.user;
+            setUser(LoggedInUser);
+            notifySignin()
 
         })
         .error(error => {
+            console.log('error', error.message);
+        })
+    }
+
+    const handleSighOut = () =>{
+        signOut(auth)
+        .then(result => {
+            setUser(null)
+            console.log(result);
+            notify()
+        })
+        .catch(error=>{
             console.log('error', error.message);
         })
     }
@@ -27,11 +55,37 @@ const Login = () => {
 
     let styles = {
         marginTop: '20px',
+       
     };
+    let styleLeft = {
+        marginLeft: '50px'
+    }
     return (
 
         <div style={styles}>
-            <button onClick={handleGoogleLogin}>Google Login 🦖</button>
+            <ToastContainer />
+
+
+            {
+             user ?
+             <button style={styleLeft} onClick={handleSighOut}>Google SignOut 🦖</button>
+             :
+             <div>
+                <button onClick={handleGoogleLogin}>Google Login 🦖</button> 
+                <button onClick={handleGithubLogin}>GitHub Login</button>
+             </div>
+            }
+            
+
+
+            { user &&  <div>
+                <h2>User Name: {user?.displayName}</h2>
+                <p>Email: {user?.email}</p>
+                <p>User Name: {user?.reloadUserInfo.screenName}</p>
+                <img src={user?.photoURL} alt="" />
+            </div>}
+
+            
         </div>
     );
 };
